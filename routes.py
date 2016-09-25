19c45e49ecbeb50d869fd8c716fab3d5bac9e3d4
Victor Pereira <vpereirabr@gmail.com>
2016-09-25 12:08:12 +0200

    first commit

---
+++ routes.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    datasets = [
+        {'filename': 'foo.csv',
+         'keywords': ['fish', 'eggs']},
+        {'filename': 'bar.csv',
+         'keywords': ['jellyfish', 'babies']}
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           datasets=datasets)
+
+if __name__ == '__main__':
+  app.run(debug=True)
